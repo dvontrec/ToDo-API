d28c9cf6a1e612d583544fe6b1061338fda9ed08
Dvontre A Coleman <dvontrec@gmail.com>
2018-03-21 07:02:17 -0400

    Setup Update and Destroy routes

--- routes/todos.js
+++ routes/todos.js
@@ -32,11 +32,40 @@ router.post("/", function(req, res){
 	//if sucessful
 	.then(function(newToDo){
 		//send the new to do back with the created status
-		res.status(201).json(newToDo);
+		res.status(201).json(newToDo); //201 is the created status.
 	})
 	.catch(function(err){
 		res.send(err);
 	})
 });
+
+//Update route
+router.put("/:todoId", function(req, res){
+	//fimds one task by the Id and updates it
+	db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new:true}) //responds with updated todo
+	//ifsuccessful
+	.then(function(todo){
+		res.json(todo);
+	})
+	//if fail
+	.catch(function(err){
+		res.send(err);
+	})
+})
+
+//Delete route
+router.delete("/:todoId", function(req, res){
+		//finds by Id and deletes
+		db.Todo.remove({_id: req.params.todoId})
+		//if successful
+		.then(function(){
+			res.json({message: "to do deleted"});
+		})
+		//if fail
+		.catch(function(err)
+		{
+			res.send(err);
+		})
+})
 
 module.exports = router;
